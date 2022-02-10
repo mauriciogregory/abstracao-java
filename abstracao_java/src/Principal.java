5c82bf4bcfed2195151648515bfdf330188cd0f1
mauriciogregory <mauricio.gre@gmail.com>
2022-02-10 20:08:14 -0300

    hello

--- abstracao_java/src/Principal.java
+++ abstracao_java/src/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 
         livro.setAutor("Machado");
         livro.setExemplares(3);
-        livro.setTitulo("Memórias de Brás Cubas.");
+        livro.setTitulo("Memórias Póstumas de Brás Cubas.");
         System.out.print(livro.getAutor());
         System.out.println("\n"+livro.getTitulo());
     }
